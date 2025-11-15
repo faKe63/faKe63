f42cc10ea68e32d4525612b69ad7f8ea1c8839f6
faKe63 <github.spoiler950@slmails.com>
2025-11-15 16:03:52 +0000

    Delete build.php

--- build.php
+++
@@ -1,7 +0,0 @@
-<?php
-// the font color using PHP, HTML and css through echo
-echo '<span style="color: red; font-size: 30px;">  <b>Em construção, </b></span>';
-echo "<br>";
-echo "<br>";
-echo '<span style="color: black; font-size: 20px;"> <b><a href="https://steamcommunity.com/profiles/76561198125112585/">byfaKe</a></b></span></span>';
-?>
